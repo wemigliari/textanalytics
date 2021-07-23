6f9ee8482fbe11e5988d5fbc596b372c3254d458
Wellington Migliari <49842956+wemigliari@users.noreply.github.com>
2021-07-23 19:11:07 +0200

    Add files via upload

--- text_analytics_tokenizer.R
+++ text_analytics_tokenizer.R
@@ -28,23 +28,64 @@ words_riks <- tokenize_words(myCorpus_pdf, strip_numeric = TRUE,
 
 tab_riks <- data.frame(matrix(unlist(words_riks), ncol = 1),stringsAsFactors=FALSE)
 
+### Tokens
+
 library(quanteda)
 # remove punctuation
-tab_riks <- as.character(tab_riks)
-tab_riks <- quanteda::tokens(tab_riks, remove_punct = TRUE)
+tab_riks_plot <- as.character(tab_riks)
+tab_riks_plot <- quanteda::tokens(tab_riks_plot, remove_punct = TRUE)
 # remove stopwords
-tab_riks <- tokens_remove(tab_riks, stopwords("english"))
-class(tab_riks)
+tab_riks_plot <- tokens_remove(tab_riks_plot, stopwords("english"))
+class(tab_riks_plot)
+head(tab_riks_plot)
 
-tab_riks <- data.frame(Tokens = as.character(tab_riks), 
+# Lexical Dispersion Plot
+
+library("quanteda.textplots")
+
+text_plot <- textplot_xray(
+  kwic(tab_riks_plot, pattern = "riksdag"),
+  kwic(tab_riks_plot, pattern = "law"),
+  kwic(tab_riks_plot, pattern = "democracy"),
+  kwic(tab_riks_plot, pattern = "government"),
+  kwic(tab_riks_plot, pattern = "powers"),
+  kwic(tab_riks_plot, pattern = "freedom"),
+  scale = "absolute"                          ### scale parameter can be "relative"
+)
+
+text_plot + aes(color = keyword) + 
+  scale_color_manual(values = c("steelblue", "lightgray", "darkgreen", "lightblue", "turquoise", "orange")) +
+  theme(legend.position = "none") +
+  ggtitle("Lexical Dispersion. Instrument of Government. English version.") +
+  labs(x = "Repetitions")
+
+
+### From tokens to depurated tokens
+
+# remove punctuation
+tab_riks3 <- as.character(tab_riks)
+tab_riks3 <- quanteda::tokens(tab_riks3, remove_punct = TRUE)
+
+# remove stopwords
+tab_riks3 <- tokens_remove(tab_riks3, stopwords("english"))
+class(tab_riks3)
+head(tab_riks3)
+
+tab_riks3 <- data.frame(Tokens = as.character(tab_riks3), 
            stringsAsFactors = FALSE)
-tab_riks$Tokens <- gsub("'s|c", '', test$Tokens) ## removing single quote
 
-tab_riks <- tab_riks[-c(1),] 
-tab_riks <- data.frame(tab_riks)
-colnames(tab_riks)[1] <- "Words"
+tab_riks3$Tokens <- gsub("'s|c", '', tab_riks3$Tokens) ## removing single quote and other characters
+tab_riks3 <- tab_riks3[-c(1),]
 
-duplicated(tab_riks) 
-tab_riks<- tab_riks %>% distinct() #Removing duplicates if that's the case
-duplicated(tab_riks)
+tab_riks3 <- data.frame(tab_riks3)
+
+colnames(tab_riks3)[1] <- "Words"
+
+#### Removing the repetitions. Only useful for a list of words.
+
+duplicated(tab_riks3) 
+tab_riks4<- tab_riks3 %>% distinct() #Removing duplicates if that's the case
+duplicated(tab_riks4)
+
+summary(tab_riks4)
 
